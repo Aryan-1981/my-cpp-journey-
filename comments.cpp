dc4a5729c7acc216f81a3f1134361d5713c8c653
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-22 23:49:47 +0530

    how to use comments

---
+++ comments.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // this program is made to define the working of comments
+    // doube // is used for single line comments
+   /* for multi-line comments like this 
+   this program is made by aryan kumar bhargava
+   and now i know how to use single line and multi line comments 
+  */
+  cout<<"i have learned how to use single and multi line comments \n";
+  return 0;
+}
