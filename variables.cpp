4b38a7c74e7fc40a0b3feb2ed2b7e8b18497448e
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-22 23:59:10 +0530

    variables

---
+++ variables.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    float b = 32;
+    char c = 'n';
+    cout<<" this is the way how you can print variables"<<endl<< a<<endl<<b<<endl<<c;
+    return 0;
+}
