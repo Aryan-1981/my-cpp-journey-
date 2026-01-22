5749c27dd2e965848bdd5bcfd505d3f8aa1e0da6
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-22 23:59:31 +0530

    edits

--- variables.cpp
+++ variables.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int a = 5;
     float b = 32;
-    char c = 'n';
+    char c = 'a';
     cout<<" this is the way how you can print variables"<<endl<< a<<endl<<b<<endl<<c;
     return 0;
 }
