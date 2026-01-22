5baa5eab4cde0019280c765f911b6f4a77e5abda
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-22 22:58:45 +0530

    2nd

---
+++ areas.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // this program is made for storing area 
+    float area = 3.4;
+    int length = 5;
+    char name = 'aryan';
+    return 0;
+}
