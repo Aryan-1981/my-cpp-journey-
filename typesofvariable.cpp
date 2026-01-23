a0a50cd4327a92e0da1f4616aa3abcfee97f4dc0
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-24 00:52:02 +0530

    local and global variables

--- typesofvariable.cpp
+++ typesofvariable.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
 using namespace std;
+int glo = 32;
+void sum(){
+    cout<<glo;
+}
 int main(){
     int a = 4;
+    int glo = 34;
     int b = 29;
     float pi = 3.14;
     char z = 'a';
+    glo = 67;
+    sum();
+    cout<<glo<<"\n";
 cout<<"this is a program to define the variables .The value of a is "<<a<<endl<<". The value of b is "<<b<<endl<<". The sum of a and b is "<<a+b<<".\n";
 cout<<"The value of pi is "<<pi<<".";
 cout<<"this line defines the character working. This prints char = "<<z<<".";
