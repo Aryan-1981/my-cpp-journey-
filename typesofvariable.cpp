c413fdf25b72078ca05459e42c81575676a0e202
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-24 00:15:08 +0530

    variables

---
+++ typesofvariable.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 4;
+    int b = 29;
+cout<<"this is a program to define the variables .The value of a is "<<a<<endl<<". The value of b is "<<b<<endl<<". The sum of a and b is "<<a+b<<".";
+return 0;
+}
