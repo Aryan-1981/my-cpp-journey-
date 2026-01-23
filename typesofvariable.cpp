3d8da80cc836545287677fa1f58215b2ce8886ad
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-24 00:18:29 +0530

    float

--- typesofvariable.cpp
+++ typesofvariable.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int main(){
     int a = 4;
     int b = 29;
+    float pi = 3.14;
 cout<<"this is a program to define the variables .The value of a is "<<a<<endl<<". The value of b is "<<b<<endl<<". The sum of a and b is "<<a+b<<".";
+cout<<"The value of pi is "<<pi<<".";
 return 0;
+
 }
