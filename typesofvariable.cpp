71dd0c0d1128a5a83b89bf807d6894154d6ef1e9
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-24 00:31:11 +0530

    char

--- typesofvariable.cpp
+++ typesofvariable.cpp
@@ -4,8 +4,10 @@ int main(){
     int a = 4;
     int b = 29;
     float pi = 3.14;
+    char z = 'a';
 cout<<"this is a program to define the variables .The value of a is "<<a<<endl<<". The value of b is "<<b<<endl<<". The sum of a and b is "<<a+b<<".\n";
 cout<<"The value of pi is "<<pi<<".";
+cout<<"this line defines the character working. This prints char = "<<z<<".";
 return 0;
 
 }
