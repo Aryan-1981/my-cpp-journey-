3b456bf03d70cae9a5ef37ad7c5ef31004f7b80f
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-24 18:35:20 +0530

    comprison operators

--- operators.cpp
+++ operators.cpp
@@ -4,18 +4,28 @@ int main()
 {
     int a = 3;
     int b = 5;
-    cout<<"operators in c++"<<endl;
-    cout<<"There are four types of operators in c++"<<endl;
-    cout<<" First one is Arthemetic Operators"<<endl;
+    cout<<"operators in c++"<<endl<<endl;
+    cout<<"There are four types of operators in c++"<<endl<<endl;
+    cout<<"First one is Arthemetic Operators"<<endl<<endl;
     // + - * /
-    cout<<" The value of a+b is: "<<a+b<<endl;
+    cout<<"The value of a+b is: "<<a+b<<endl;
     cout<<"The value of a-b is: "<<a-b<<endl;
     cout<<"The value of a*b is: "<<a*b<<endl;
-    cout<<"THe value of a/b is :"<<a%b<<endl;
-    return 0;
+    cout<<"THe value of a/b is :"<<a%b<<endl<<endl;
     // Assignment operators used to assign values to variables
+     cout<<"Second one are the Assignment Operators"<<endl<<endl;
     int c= 3;
     int d=23;
     char f = 'a';
-    
+    cout<<"Print the values of int c, d, and character f  are  "<<c<<"  "<<d<<"  "<<f<<endl<<endl;
+    //Camparison Operatos
+    cout<<"Third one is comparison Operators"<<endl<<endl;
+    cout<<"The value of a==b is :"<<(a==b)<<endl;
+    cout<<"The value of a!=b is :"<<(a!=b)<<endl;
+    cout<<"The value of a>=b is :"<<(a>=b)<<endl;
+    cout<<"The value of a<=b is :"<<(a<=b)<<endl;
+    cout<<"The value of a>b is :"<<(a>b)<<endl;
+    cout<<"The value of a<b is :"<<(a<b)<<endl;
+    return 0;
+
 }
