5c403cd25c5399c8682ed007a7040477502ef64b
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-24 14:59:06 +0530

    updated

--- inputoutput.cpp
+++ inputoutput.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int num1 , num2 ;
-cout<<"Enter a number"<<num1<<".\n";
-cin>>num1;
+cout<<"Enter a number\n"; // << is called insertion operators
+cin>>num1;    // >> is called extraction operators
 return 0;
 }
