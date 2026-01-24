6dd7e56d27249e2e1b2817886e6d2fa6e967072d
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-24 15:02:59 +0530

    sum of num1 and 2

--- inputoutput.cpp
+++ inputoutput.cpp
@@ -8,5 +8,7 @@ cin>>num1;    // >> is called extraction operators
 
 cout<<"Enter a number for num2 \n"; // << is called insertion operators
 cin>>num2;    // >> is called extraction operators
+
+cout<<"The sum of num1 and num2 is "<<num1+num2<<".";
 return 0;
 }
