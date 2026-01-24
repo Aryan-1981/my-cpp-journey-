ac6b157814d2c398caf9367498b645dd2fde1e9d
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-24 17:54:23 +0530

    arthemetic operators

---
+++ operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 3;
+    int b = 5;
+    cout<<"operators in c++"<<endl;
+    cout<<"There are four types of operators in c++"<<endl;
+    cout<<" First one is Arthemetic Operators"<<endl;
+    // + - * /
+    cout<<" The value of a+b is: "<<a+b<<endl;
+    cout<<"The value of a-b is: "<<a-b<<endl;
+    cout<<"The value of a*b is: "<<a*b<<endl;
+    cout<<"THe value of a/b is :"<<a/b<<endl;
+    
+}
