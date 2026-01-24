0084c94213cbbbfad635948c402ceba374877c5a
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-24 17:40:38 +0530

    header

---
+++ headerfiles.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"This program is to define header files";
+    //There are two types of header files 
+    // 1. First is System header file: It comes with the compiler 
+    // 2. Seconf is User defined header file : it is written by the programmer
+}
