d92c588c12174ffbee7485dcdc8e8b21c4cc6140
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-27 21:52:27 +0530

    typecasting

---
+++ typecasting.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 43;
+    float b = 43.43;
+    cout<<"The value of a is " <<a<<endl;
+    cout<<" the value of float a is "<<(float)a<<endl;
+    cout<<"The value of b is "<<b<<endl;
+    cout<<"The value of in b is "<<(int)b<<endl;
+    return 0;
+}
