b3d38cf2efe5d82c10b0fa881f21609fc66c9da5
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-27 21:38:28 +0530

    refrencevariables

---
+++ refrence_variables.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a = 432;
+    float & b = a;
+    cout<<"The value of a and b are as follows:\n"<<a<<endl<<b<<endl;
+    return 0;
+
+}
