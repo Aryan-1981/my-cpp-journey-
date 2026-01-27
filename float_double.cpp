283ba71d65b87348222d84cfc933700cd47e7011
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-27 20:56:24 +0530

    sizes of float and double

--- float_double.cpp
+++ float_double.cpp
@@ -3,7 +3,14 @@ using namespace std;
 int main() {
      float a= 32.32f;
      long double b = 32.32l;
-     cout<<"The value of a is "<<a<<". The value of b is "<<b;
+     cout<<"The value of a is "<<a<<". The value of b is "<<b<<endl;
+     cout<<" The size of 32.32 is "<<sizeof(32.32)<<endl;
+     cout<<" The size of 32.32f is "<<sizeof(32.32f)<<endl;
+     cout<<" The size of 32.32F is "<<sizeof(32.32F)<<endl;
+     cout<<" The size of 32.32l is "<<sizeof(32.32l)<<endl;
+     cout<<" The size of 32.32L is "<<sizeof(32.32L)<<endl;
+     
+
      return 0;
 
 }
