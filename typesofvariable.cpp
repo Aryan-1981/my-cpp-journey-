99c4e56f7181029b4d36b2071cee227f702fd315
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-27 20:02:47 +0530

    edits

--- typesofvariable.cpp
+++ typesofvariable.cpp
@@ -13,8 +13,9 @@ int main(){
     glo = 67;
     sum();
     cout<<glo<<"\n";
-cout<<"this is a program to define the variables .The value of a is "<<a<<endl;
-cout<< " The value of b is "<<b<<endl<<". The sum of a and b is "<<a+b<<".\n";
+cout<<"this is a program to define the variables . "<<endl;
+cout<<" value of a is "<<a<<endl;
+cout<< " The value of b is "<<b<<endl<<"\n.The sum of a and b is "<<a+b<<".\n";
 cout<<"The value of pi is "<<pi<<".";
 cout<<"this line defines the character working. This prints char = "<<z<<".";
 return 0;
