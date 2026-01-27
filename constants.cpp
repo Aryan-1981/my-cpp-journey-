fa7ba939468193baf12fc0c1882a0dd8f91ec0dc
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-27 22:09:40 +0530

    change value of variables

---
+++ constants.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 32;
+    cout<<"The value of int a was "<<a<<endl;
+    a = 26;
+    cout<<"The value of int a is "<<a<<endl;
+    // The value changed
+    return 0;
+}
