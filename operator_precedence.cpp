faea4cac627347a95c2b977facf29de6037c1094
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-27 23:07:03 +0530

    comments

--- operator_precedence.cpp
+++ operator_precedence.cpp
@@ -5,6 +5,6 @@ int main()
     int a = 43;
     int b= 64;
     int c = ((((a*b)+43)-46)+56); // This shows the working of operator precedence .
-    cout<<c;
+    cout<<"THe value of c will be "<<c;
     return 0;
 }
