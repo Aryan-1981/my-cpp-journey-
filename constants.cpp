73b15b7ee6414b80f0bcbde8408fc320a1e76bf2
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-27 22:11:32 +0530

    the working of cont

--- constants.cpp
+++ constants.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 int main()
 {
-    int a = 32;
+    // int a = 32;
+    // cout<<"The value of int a was "<<a<<endl;
+    // a = 26;
+    // cout<<"The value of int a is "<<a<<endl;
+    // // The value changed
+
+    const int a = 43;
     cout<<"The value of int a was "<<a<<endl;
-    a = 26;
-    cout<<"The value of int a is "<<a<<endl;
-    // The value changed
+     a = 26;
+     cout<<"The value of int a is "<<a<<endl;
+    //  now it is giving error if we have written const in it 
+
     return 0;
 }
