65c33acb6ebe85350ebdba61fbb9b9303989a579
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-27 20:50:58 +0530

    float and double

---
+++ float_double.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+     float a= 32.32f;
+     long double b = 32.32l;
+     cout<<"The value of a is "<<a<<". The value of b is "<<b;
+     return 0;
+
+}
