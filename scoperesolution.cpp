1a7c7bd7645738e0e5f5a1ffb042b604c4042d81
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-01-27 20:29:47 +0530

    edits

--- scoperesolution.cpp
+++ scoperesolution.cpp
@@ -4,9 +4,9 @@ int c = 45;
 int main()
 {
     int a,b,c;
-    cout<<"Enter a number to be assign with a";
+    cout<<"Enter a number to be assign with a"<<endl;
     cin>>a;
-    cout<<"Enter a number to be assign with b";
+    cout<<"Enter a number to be assign with b"<<endl;
     cin>>b;
     c=a+b;
     cout<<"The sum of a and b is "<<c<<"."<<endl;
